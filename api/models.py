f44c1fa0f634c990ce098a65c9775a252091c5ee
Syntaxamurai <justinabwunza@gmail.com>
2026-06-19 14:57:15 +0300

    add loan model

--- api/models.py
+++ api/models.py
@@ -9,3 +9,29 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class Loan(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('paid', 'Paid'),
+        ('defaulted', 'Defaulted'),
+    ]
+
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='loans'
+        )
+    principal_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=4)
+    term_months = models.IntegerField()
+    status = models.CharField(
+        max_length=20, 
+        choices=STATUS_CHOICES, 
+        default='active'
+    )
+    disbursed_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Loan {self.id}: {self.user.email} - Kshs. {self.principal_amount}"
